e215f23e8a0e153d46a10616d14dcb40f688779a
msaad1311 <msaad1311@users.noreply.github.com>
2020-09-27 00:05:05 -0400

    The html file name in app.py is changed to home and new requiremnt file is added
    The name of the html file was initially Untitled which is altered to home.html and the a new environment is created and the requirement file is generated from there

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ model = pickle.load(open("flight_fare.pkl", "rb"))
 @app.route("/")
 @cross_origin()
 def home():
-    return render_template("Untitled.html")
+    return render_template("home.html")
 
 
 @app.route("/predict", methods=["GET", "POST"])
@@ -360,9 +360,9 @@ def predict():
 
         output = round(prediction[0], 2)
 
-        return render_template('Untitled.html', prediction_text="Your Flight price is Rs. {}".format(output))
+        return render_template('home.html', prediction_text="Your Flight price is Rs. {}".format(output))
 
-    return render_template("Untitled.html")
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
